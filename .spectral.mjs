62ed792cf3d73b849fdb1b30f3604236dbd02bbe
Aslam <desusa77777@gmail.com>
2024-05-22 11:47:46 +0530

    advertiser api docs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NzE5Mw/spectral.js?branch_slug=master&token=639a6dbf-3082-45f6-8b8c-f1f2133d346a";
+export default { extends: ruleset };
